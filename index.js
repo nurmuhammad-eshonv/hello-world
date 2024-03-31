a1997d5657486d9936e4beee2473e8e219f02f8a
nurmuhammad <nurmuhammadeshonov97@gmail.com>
2024-03-31 10:24:35 +0500

    added the code

---
+++ index.js
@@ -0,0 +1,6 @@
+let everythink = prompt('Qancha marta "Hello World" sozini chiqarmoqchisiz?: ');
+
+for (let i = 0; i < everythink; i++) {
+  console.log("Hello World1");
+  document.write("hello-world ");
+}
